87f7a40204f3e85ed66739871a69e2e7162fa191
Ren Tatsumoto <tatsu@autistici.org>
2021-04-20 13:31:51 +0300

    support import/export when invoked as a module

--- antp/__main__.py
+++ antp/__main__.py
@@ -1,10 +1,24 @@
 from urllib.error import URLError
-
+import sys
 from antp.exporter import export_note_type
+from antp.importer import import_note_type
 
-try:
-    export_note_type()
-except URLError:
-    print("Couldn't connect. Make sure Anki is open and AnkiConnect is installed.")
-except Exception as ex:
-    print(ex)
+
+def main():
+    try:
+        if len(sys.argv) < 2:
+            print("No action provided.")
+            return
+        cmd = sys.argv[1]
+        if cmd == 'export':
+            export_note_type()
+        elif cmd == 'import':
+            import_note_type()
+    except URLError:
+        print("Couldn't connect. Make sure Anki is open and AnkiConnect is installed.")
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == '__main__':
+    main()
