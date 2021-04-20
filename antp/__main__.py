afd62cff14b913728c641d29ccf2b013b6ea445d
Ren Tatsumoto <tatsu@autistici.org>
2021-04-20 12:27:48 +0300

    add main

---
+++ antp/__main__.py
@@ -0,0 +1,10 @@
+from urllib.error import URLError
+
+from antp.template_picker import export_note_type
+
+try:
+    export_note_type()
+except URLError:
+    print("Couldn't connect. Make sure Anki is open and AnkiConnect is installed.")
+except Exception as ex:
+    print(ex)
