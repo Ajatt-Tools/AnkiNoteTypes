d4bd674c0610bc00f09073ff1052c2c865c09bee
Ren Tatsumoto <tatsu@autistici.org>
2022-04-03 17:57:00 +0300

    add verbose option

--- antp/__main__.py
+++ antp/__main__.py
@@ -2,6 +2,7 @@
 # License: GNU GPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 import sys
+from typing import Callable
 from urllib.error import URLError
 
 from .common import ANTPError
@@ -10,31 +11,60 @@ from .importer import import_note_type
 from .updater import update_note_type
 
 
-def main():
-    if len(sys.argv) < 2:
-        return print(
-            "No action provided.\n\n"
-            f"import\tAdd one of the available note types to Anki.\n"
-            f"update\tOverwrite a previously imported note type with new data.\n"
-            f"export\tSave your note type as a template.\n"
-        )
-
+def wrap_errors(fn: Callable):
     try:
-        match sys.argv[1]:
-            case 'export':
-                export_note_type()
-            case 'import':
-                import_note_type()
-            case 'update':
-                update_note_type()
-            case _:
-                print("Unknown action.")
+        fn()
     except URLError:
         print("Couldn't connect. Make sure Anki is open and AnkiConnect is installed.")
     except ANTPError as ex:
         print(ex)
     except KeyboardInterrupt:
         print("\nAborted.")
+
+
+def print_help():
+    options = (
+        ("import", "Add one of the available note types to Anki."),
+        ("update", "Overwrite a previously imported note type with new data."),
+        ("export", "Save your note type as a template."),
+        ("-v, --verbose", "Show detailed info when errors occur."),
+    )
+    print(
+        "Usage: antp.sh [OPTIONS]\n\n"
+        "Options:"
+    )
+    col_width = [max(len(word) for word in col) + 2 for col in zip(*options)]
+    for row in options:
+        print(" " * 4, "".join(col.ljust(col_width[i]) for i, col in enumerate(row)), sep='')
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("No action provided.")
+        print_help()
+        return
+
+    action = None
+    wrap = True
+
+    for arg in sys.argv[1:]:
+        match arg:
+            case 'export':
+                action = export_note_type
+            case 'import':
+                action = import_note_type
+            case 'update':
+                action = update_note_type
+            case '-v' | '--verbose':
+                wrap = False
+
+    if action and wrap:
+        wrap_errors(action)
+    elif action:
+        action()
+    else:
+        print("Unknown action.")
+        print_help()
 
 
 if __name__ == '__main__':
