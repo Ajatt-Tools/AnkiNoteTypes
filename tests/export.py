5901f54200ec0f10901e29efe5fed99912287c6a
Ren Tatsumoto <tatsu@autistici.org>
2024-08-29 21:28:34 +0300

    remove antp

--- tests/export.py
+++
@@ -1,4 +0,0 @@
-from antp.exporter import export_note_type
-
-if __name__ == '__main__':
-    export_note_type()
--- tests/import.py
+++
@@ -1,4 +0,0 @@
-from antp.importer import import_note_type
-
-if __name__ == '__main__':
-    import_note_type()
